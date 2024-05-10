bd23530f70315ddf53e92c1e8a8e7da20ae1b49d
cui xingfan <cuixingfan@reddatetech.com>
2024-05-10 14:14:28 +0800

    问

--- src/main/java/com/zeroone/libicc/LibICC.java
+++ src/main/java/com/zeroone/libicc/LibICC.java
@@ -295,8 +295,8 @@ public class LibICC implements AutoCloseable {
     }
 
     static {
-        String lib = System.getenv("lib");
-        System.load(lib);
+//        String lib = System.getenv("lib");
+//        System.load(lib);
 //        System.loadLibrary("E:\\icron-icc-workspace\\icron-java\\src\\main\\java\\libs\\x86_64\\libicc.so");
         System.loadLibrary("icc-jni");
         libInit();

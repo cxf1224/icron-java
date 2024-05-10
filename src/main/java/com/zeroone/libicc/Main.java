a4424eabb1710eb9dd80ba19ddfab3f0a88e24dd
cui xingfan <cuixingfan@reddatetech.com>
2024-05-10 14:18:13 +0800

    问

--- src/main/java/com/zeroone/libicc/Main.java
+++ src/main/java/com/zeroone/libicc/Main.java
@@ -3,10 +3,10 @@ package com.zeroone.libicc;
 public class Main {
 
   public static void main(String[] args) {
-    String lib = System.getenv("lib");
-    System.out.println("lib >>>>>>>>>"+lib);
-    String libPath = System.getProperty("java.library.path");
-    System.out.println("libPath >>>>>>>>> "+libPath);
+//    String lib = System.getenv("lib");
+//    System.out.println("lib >>>>>>>>>"+lib);
+//    String libPath = System.getProperty("java.library.path");
+//    System.out.println("libPath >>>>>>>>> "+libPath);
 
   }
 }
